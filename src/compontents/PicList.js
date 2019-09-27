26f4bf821fe89e4ae806b16c9b8c3fba44fce128
FreedomWriter <natalienetworks@gmail.com>
2019-09-26 20:44:05 -0500

    mvp

--- src/compontents/PicList.js
+++ src/compontents/PicList.js
@@ -10,19 +10,11 @@ export default function PicList() {
       .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
       .then(result => {
         setPics(result.data);
-
-        // console.log(result.data);
-        // console.log(result.data.title);
-        // console.log(
-        //   `Date: ${result.data.date} Explanation: ${result.data.explanation}, hdurl: ${result.data.hdurl},  Media Type: ${result.data.media_type} Service Version: ${result.data.v1} Title: ${result.data.title} url: ${result.data.url}`
-        // );
       })
       .catch(err => {
         console.log(err);
       });
   }, []);
-
-  //   console.log();
   return (
     <div>
       <PicCard
